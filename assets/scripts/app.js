0afbe81fbbec54396b12798f11c61ea85b67f5b1
Bigg-Iron <lorenzor.bartolo@Lorenzos-MacBook-Pro.local>
2017-09-10 21:12:04 -0700

    Fixed giphy population

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,10 +9,9 @@ $("#cat").on("click", function () {
             url: queryURL,
             method: "GET"
     })
-
         // callback
         .done(function (response) {
-
+            console.log(response);
             //
             var imageUrl = response.data.image_original_url;
 
@@ -45,7 +44,7 @@ $("#dog").on("click", function () {
 
         // callback
         .done(function (response) {
-
+            console.log(response);
             //
             var imageUrl = response.data.image_original_url;
 
@@ -77,7 +76,7 @@ $("#bird").on("click", function () {
 
         // callback
         .done(function (response) {
-
+            console.log(response);
             //
             var imageUrl = response.data.image_original_url;
 
@@ -96,48 +95,3 @@ $("#bird").on("click", function () {
 
 
 
-
-
-// $("button").on("click", function () {
-//     var animal = $(this).attr("data-animal");
-//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-//         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).done(function (response) {
-
-//         // since the image information is inside of the data key...
-//         var results = response.data; 
-//         console.log(results);
-//         $("#animals").append(" cat giphy ");
-
-
-
-
-//         for (var i = 0; i < results.length; i++) {
-
-//             var animalDiv = $("<div>");
-//             var p = $("<p>").text;
-
-// TODO:
-// Make a div with jQuery and store it in a variable named animalDiv.
-// Make a paragraph tag with jQuery and store it in a variable named p.
-// Set the inner text of the paragraph to the rating of the image in results[i].
-// Make an image tag with jQuery and store it in a variable named animalImage.
-// Set the image's src to results[i]'s fixed_height.url.
-// Append the p variable to the animalDiv variable.
-// Append the animalImage variable to the animalDiv variable.
-// Prepend the animalDiv variable to the element with an id of gifs-appear-here.
-
-
-//         };
-
-
-
-
-
-
-//     });
-// });
